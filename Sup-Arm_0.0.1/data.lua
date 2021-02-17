0fa9fe5e03932372832f505b3c2637f1b979b1a5
DemonSF <DemonSF01@mail.ru>
2021-02-17 21:03:27 +0500

    DemonSF

--- Sup-Arm_0.0.1/data.lua
+++ Sup-Arm_0.0.1/data.lua
@@ -1,8 +1,3 @@
-require("prototypes.technology")
-require("prototypes.entity")
---require("prototypes.entity-deployer")
-require("prototypes.group")
-require("prototypes.item")
-require("prototypes.recipe")
-require("prototypes.gun")
+require("technology.tech")
+
 
